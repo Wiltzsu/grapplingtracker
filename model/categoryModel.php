c2cb9a9c9c3be795b81b3925a4cddf0255d31dcd
Wiltzsu <william.lonnberg@gmail.com>
2024-05-21 14:18:51 +0300

    category model adding hints

--- model/categoryModel.php
+++ model/categoryModel.php
@@ -3,7 +3,7 @@
  * Category model for interacting with the 'Category' table in the database.
  * 
  * @package Techniquedbmvc
- * @autor   William Lönnberg <william.lonnberg@gmail.com>
+ * @author   William Lönnberg <william.lonnberg@gmail.com>
  * @license MIT License
  */
 
@@ -20,14 +20,14 @@ class Category
     /**
      * @var PDO Database connection
      */
-    private $_db;
+    private PDO $_db; // Ensures that $_db is always an instance of PDO.
 
     /**
      * Initializes the Category model with a database connection.
      * 
      * @param PDO $db Database connection
      */
-    public function __construct($db)
+    public function __construct(PDO $db) // Ensures that the constructor can only be called with an instance of PDO.
     {
         $this->_db = $db;
     }
@@ -37,7 +37,7 @@ class Category
      * 
      * @return array Associative array of categories
      */
-    public function getAllCategories()
+    public function getAllCategories(): array // Ensures that getAllCategories always returns an array.
     {
         $statement = $this->_db->prepare("SELECT * FROM Category");
         $statement->execute();
