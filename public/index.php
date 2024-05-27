2888460ec9a68d8069a61c40c8bac3da9463c65a
Wiltzsu <william.lonnberg@gmail.com>
2024-05-27 16:15:08 +0300

    main controller

--- public/index.php
+++ public/index.php
@@ -13,16 +13,15 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 
-// Load Composer's autoload to manage dependencies and class loading.
+// Load Composer's autoload to manage dependencies and class loading
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Use namespaces to organize and reference the MainController and Database classes.
 use App\Controller\MainController;
 use App\Config\Database;
 
-// Connect to the database and store the connection in $db.
+// Connect to the database and store the connection in $db
 $db = Database::connect();
 
-// Create an instance of MainController with the database connection and execute the main logic.
 $mainController = new MainController($db);
 $mainController->index();
---
+++ src/Model/MainController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Controls the flow between the user interface and the model data processing,
+ * coordinating responses based on user actions and input.
+ *
+ * @package Techniquedbmvc
+ * @author  William
+ * @license MIT License
+ */
+
+namespace App\Controller;
+
+use App\Model\CategoryModel;
+use App\Model\DifficultyModel;
+use App\Model\PositionModel;
+
+class MainController
+{
+    private $_db;
+    private $categoryModel;
+    private $difficultyModel;
+    private $positionModel;
+
+    /**
+     * Initializes the controller with model instances using a database connection.
+     *
+     * @param \PDO $db Database connection
+     */
+    public function __construct(\PDO $db)
+    {
+        $this->_db = $db;
+        $this->categoryModel = new CategoryModel($this->_db);
+        $this->difficultyModel = new DifficultyModel($this->_db);
+        $this->positionModel = new PositionModel($this->_db);
+    }
+
+    /**
+     * Gathers data from models and passes them to the view for rendering.
+     * This is the main entry point for rendering the page requested by the user.
+     */
+    public function index()
+    {
+        $categories = $this->categoryModel->getAllCategories();
+        $difficulties = $this->difficultyModel->getAllDifficulties();
+        $positions = $this->positionModel->getAllPositions();
+
+        include __DIR__ . "/../View/add_new.php";
+    }
+}
