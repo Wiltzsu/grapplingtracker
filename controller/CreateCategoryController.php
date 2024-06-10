0157de5e9fe96231ef87a8886ac10e4f20e05e6b
Wiltzsu <william.lonnberg@gmail.com>
2024-06-10 17:34:07 +0300

    createcategorycontroller

--- controller/CreateCategoryController.php
+++ controller/CreateCategoryController.php
@@ -14,4 +14,68 @@ class CreateCategoryController
     /**
      * Initialize with a database connection.
      */
+    public function __construct($db)
+    {
+        $this->_categoryModel = new Category($db);
+    }
+
+    /**
+     * Uses the instance of the category model and adds the category to the database.
+     * 
+     * @param string $categoryName          Name of the category.
+     * @param string $categoryDescription   Category description.
+     * 
+     * @return Array Returns an empty array if successful or an array containing errors.
+     */
+    public function createCategory(
+        $categoryName,
+        $categoryDescription)
+    {
+        return $this->_categoryModel->addCategory(
+            $categoryName,
+            $categoryDescription
+        );
+    }
+}
+
+/**
+ * Initialize empty arrays to store errors and input data.
+ */
+$errors = [
+    'emptyField' => ''
+];
+
+$input = [
+    'categoryName' => '',
+    'categoryDescription' => ''
+];
+
+/**
+ * Initialize the database connection.
+ * Instantiate the class.
+ */
+$db = Database::connect();
+$createCategoryController = new CreateCategoryController($db);
+
+/**
+ * Check if form is submitted and retrieve input data.
+ * '??' provides an empty default string.
+ */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $categoryName = $_POST['categoryName'];
+    $categoryDescription = $_POST['categoryDescription'];
+
+    /**
+     * Execute the function.
+     * If no errors, redirect to 'addnew' page.
+     */
+    $errors = $createCategoryController->createCategory(
+        $categoryName,
+        $categoryDescription
+    );
+
+    if (empty($errors)) {
+        header("Location: /technique-db-mvc/view/add_new.php");
+        exit();
+    }
 }
--- controller/CreateTechniqueController.php
+++ controller/CreateTechniqueController.php
@@ -34,7 +34,7 @@ class CreateTechniqueController
     }
 
     /**
-     * Uses instance of the model and adds the technique to the database.
+     * Uses the instance of the model and adds the technique to the database.
      * 
      * @param string $techniqueName        Name of the technique.
      * @param string $techniqueDescription Description of the technique.
@@ -44,20 +44,43 @@ class CreateTechniqueController
      * 
      * @return Array Returns an empty array if successful or an array containing errors.
      */
-    public function createTechnique($techniqueName, $techniqueDescription, $categoryID, $positionID, $difficultyID)
-    {
-        return $this->_techniqueModel->addTechnique($techniqueName, $techniqueDescription, $categoryID, $positionID, $difficultyID);
+    public function createTechnique(
+        $techniqueName,
+        $techniqueDescription,
+        $categoryID,
+        $positionID,
+        $difficultyID
+    ) {
+        return $this->_techniqueModel->addTechnique(
+            $techniqueName,
+            $techniqueDescription,
+            $categoryID,
+            $positionID,
+            $difficultyID
+        );
     }
 }
 
-// Initialize empty arrays to store errors and input data.
-$errors = ['emptyField' => ''];
-$input = ['techniqueName' => '', 'techniqueDescription' => '', 'categoryID' => '', 'positionID' => '', 'difficultyID' => ''];
+/**
+ * Initialize empty arrays to store errors and input data.
+ */
+$errors = [
+    'emptyField' => ''
+];
 
-// Initializes the database connection.
+$input = [
+    'techniqueName' => '',
+    'techniqueDescription' => '',
+    'categoryID' => '',
+    'positionID' => '',
+    'difficultyID' => ''
+];
+
+/**
+ * Initialize the database connection.
+ * Instantiate the class.
+ */
 $db = Database::connect();
-
-// Instantiates the class.
 $createTechniqueController = new CreateTechniqueController($db);
 
 /**
@@ -73,14 +96,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     /**
      * Executes the creation of a technique.
-     * If no errors, redirect to addnew page.
+     * If no errors, redirect to 'addnew' page.
      */
-    $errors = $createTechniqueController->createTechnique($techniqueName, $techniqueDescription, $categoryID, $positionID, $difficultyID);
+    $errors = $createTechniqueController->createTechnique(
+        $techniqueName,
+        $techniqueDescription,
+        $categoryID,
+        $positionID,
+        $difficultyID
+    );
 
     if (empty($errors)) {
         header("Location: /technique-db-mvc/view/add_new.php");
         exit();
-    } else {
-        $error_message = join('<br>', $errors);
     }
 }
