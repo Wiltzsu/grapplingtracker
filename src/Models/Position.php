2e5838ab513442a29c3164358b93b438e396d27a
William <william.lonnberg@gmail.com>
2024-08-19 21:38:05 +0300

    Edit Position model

--- src/Models/Position.php
+++ src/Models/Position.php
@@ -1,19 +1,49 @@
 <?php
-
+/**
+ * This file contains the Position model.
+ * 
+ * PHP version 8.0.0
+ * 
+ * @category Models
+ * @package  App\Models
+ * @author   William Lönnberg <william.lonnberg@gmail.com>
+ * @license  MIT License
+ * @link     https://grapplingtracker.com
+ */
 namespace App\Models;
 
 use PDO;
+
+/**
+ * The Position model.
+ * 
+ * @category Models
+ * @package  App\Models
+ * @author   William Lönnberg <william.lonnberg@mail.com>
+ * @license  MIT License
+ * @link     https://grapplingtracker.com
+ */
 class Position
 {
     private $db;
     private $positionName;
     private $positionDescription;
 
+    /**
+     * Constructor for the Position model.
+     * 
+     * @param PDO $db The database connection.
+     */
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
+    /**
+     * Get all positions from the database.
+     * 
+     * @return array An array of all positions.
+     */
     public function getPositions()
     {
         $statement = $this->db->prepare("SELECT * FROM Position");
@@ -21,25 +51,41 @@ class Position
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function validateCreatePosition($positionName, $positionDescription)
+    /**
+     * Validate the input for creating a new position.
+     * 
+     * @param string $positionName        The name of the position.
+     * @param string $positionDescription The description of the position.
+     * 
+     * @return array An array of errors.
+     */
+    public function validateCreatePosition($positionName, $positionDescription): array
     {
         $errors = [];
 
         if (empty($positionName)) {
             $errors['positionName'] = 'Add a name for the position.';
-        } else if (!preg_match("/^[a-zA-Z\s]+$/", $positionName)) {
-            $errors['positionName'] = 'Position name can contain only letters and spaces.';
+        } else if (strlen($positionName) > 50) {
+            $errors['positionName'] = 'Position name is too long.';
         }
 
         if (empty($positionDescription)) {
             $errors['positionDescription'] = 'Add a description for the position.';
-        } else if (!preg_match("/^[a-zA-Z0-9\s]+$/", $positionDescription)) {
-            $errors['positionDescription'] = 'Position description can contain only letters, numbers and spaces.';
+        } else if (strlen($positionDescription) > 255) {
+            $errors['positionDescription'] = 'Position description is too long.';
         }
 
         return $errors;
     }
 
+    /**
+     * Create a new position in the database.
+     * 
+     * @param string $positionName        The name of the position.
+     * @param string $positionDescription The description of the position.
+     * 
+     * @return bool True if the position was created, false otherwise.
+     */
     public function createNewPosition($positionName, $positionDescription) :bool
     {
         $this->positionName = $positionName;
