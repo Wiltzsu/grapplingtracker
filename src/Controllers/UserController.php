dc3c2a7d02090b6563509d8ec204d0fc0d61aab6
William <william.lonnberg@gmail.com>
2024-10-06 17:36:12 +0300

    Change logout controller path and adjust login input width

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -210,6 +210,6 @@ class UserController
         session_destroy();
 
         // Redirect to home page.
-        header("Location: /grappling-tracker/public/");
+        header("Location: /");
     }
 }
